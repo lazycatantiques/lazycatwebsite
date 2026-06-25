a52779516f31de0f8e123140489ef8eefd91a8e5
lazycatantiques <lazycatantiques@outlook.com>
2026-06-25 17:26:02 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,21 +7,6 @@ menuBtn.onclick = () =>{
    navbar.classList.toggle('active');
 };
 
-
-let menuBtn = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
-
-menuBtn.onclick = () => {
-    menuBtn.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
-window.onscroll = () => {
-    menuBtn.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
-
-
 window.onscroll = () =>{
    menuBtn.classList.remove('fa-times');
    navbar.classList.remove('active');
